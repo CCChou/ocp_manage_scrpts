93e600458435930a93bf3beb9894b5ec68b3bfe9
dennischou <dses5900@gmail.com>
2021-01-14 14:33:32 +0800

    Refactoring extract variable

--- clean_registry/clean_registry.py
+++ clean_registry/clean_registry.py
@@ -1,47 +1,49 @@
 import httplib
 import json
 
-def main():
-    URL = 'localhost'
-    PORT = 5000
+HOST = 'localhost'
+PORT = 5000
+conn = None
 
-    conn = getconnection(URL, PORT)
-    repos = getrepos(conn)
-    if repos == None:
+def main():
+    initconnection()
+
+    repos = getrepos()
+    if repos is None:
         print('no repositories found')
         conn.close()
         return
     print('find %s repositories' % len(repos))
 
     for repo in repos:
-        tags = gettags(conn, repo)
-        if tags == None:
+        tags = gettags(repo)
+        if tags is None:
             continue
         print('find %s in %s repository' % (len(tags), repo))
 
         for tag in tags:
-            digest = getdigest(conn, repo, tag)
+            digest = getdigest(repo, tag)
             print('deleting image "%s/%s" with manifest "%s"' % (repo, tag, digest))
             msg = deleteimage(conn, repo, digest)
             print('deletion %s' % msg)
             
     conn.close() 
 
-def getconnection(url, port):
-    return httplib.HTTPConnection(url, port)
+def initconnection():
+    conn = httplib.HTTPConnection(HOST, PORT)
 
-def getrepos(conn):
+def getrepos():
     conn.request('GET', '/v2/_catalog')
     res = conn.getresponse()
     data = res.read()
     return json.loads(data)['repositories']
 
-def gettags(conn, repo):
+def gettags(repo):
     conn.request('GET', '/v2/' + repo + '/tags/list')
     res = conn.getresponse()
     return json.loads(res.read())['tags']
 
-def getdigest(conn, repo, tag):
+def getdigest(repo, tag):
     headers = {'Accept': 'application/vnd.docker.distribution.manifest.v2+json'}
     url = '/v2/%s/manifests/%s' % (repo, tag)
     conn.request('GET', url, headers=headers)
@@ -49,7 +51,7 @@ def getdigest(conn, repo, tag):
     res.read()
     return res.getheader('docker-content-digest')
 
-def deleteimage(conn, repo, digest):
+def deleteimage(repo, digest):
     headers = {'Accept': 'application/vnd.docker.distribution.manifest.v2+json'}
     url = '/v2/%s/manifests/%s' % (repo, digest)
     conn.request('DELETE', url, headers=headers)
