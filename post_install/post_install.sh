3fdd78f78ed4b3a9034c7a8be677401744f7b240
dennischou <dses5900@gmail.com>
2021-01-20 16:41:52 +0800

    Formatting the coding style

--- post_install/post_install.sh
+++ post_install/post_install.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] ; then
-    echo "Usage: post_install.sh [username] [password]"
-    exit 1
+if [ "$#" -ne 2 ] ; then
+  echo "Usage: post_install.sh [username] [password]"
+  exit 1
 fi
 
 TMP_PATH=/tmp/htpasswd
-USER=$1
-PWD=$2
+USER="$1"
+PWD="$2"
 
-InstallCompletion() {
-    echo "Start installing completion..."
+install_completion() {
+  echo "Start installing completion..."
 
-    yum -y install bash-completion
-    oc completion bash > /etc/bash_completion.d/oc_completion
-    source /usr/share/bash-completion/bash_completion
-    source /etc/bash_completion.d/oc_completion
+  yum -y install bash-completion
+  oc completion bash > /etc/bash_completion.d/oc_completion
+  source /usr/share/bash-completion/bash_completion
+  source /etc/bash_completion.d/oc_completion
 }
 
+add_default_user() {
+  echo "Start adding user ..."
 
-AddDefaultUser() {
-    echo "Start adding user ..."
-
-    htpasswd -c -B -b $TMP_PATH $USER $PWD
-    oc create secret generic htpass-secret --from-file=htpasswd=$TMP_PATH -n openshift-config
-    oc apply -f - << EOF
+  htpasswd -c -B -b "${TMP_PATH $USER $PWD}"
+  oc create secret generic htpass-secret --from-file=htpasswd="${TMP_PATH}" -n openshift-config
+  oc apply -f - << EOF
 apiVersion: config.openshift.io/v1
 kind: OAuth
 metadata:
@@ -39,8 +38,8 @@ spec:
           name: htpass-secret
 EOF
 
-    oc adm policy add-cluster-role-to-user cluster-admin $USER
+  oc adm policy add-cluster-role-to-user cluster-admin "${USER}"
 }
 
-InstallCompletion
-AddDefaultUser
+install_completion
+add_default_user
