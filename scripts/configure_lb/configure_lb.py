e4d160873bd7bfac854e91abf81e2ac0617b4fa8
dennischou <dses5900@gmail.com>
2021-01-26 21:40:02 +0800

    Fix Bugs 1. Add haproxy install, enable, start 2. find template by current path

--- scripts/configure_lb/configure_lb.py
+++ scripts/configure_lb/configure_lb.py
@@ -2,6 +2,8 @@ import json
 import sys
 import os
 import shutil
+import subprocess
+import glob
 from scripts.common.constant import Node
 
 
@@ -25,31 +27,34 @@ def main():
         return
 
     config = read_config(sys.argv[1])
-    nodes = config['nodes']
-    worker_nodes = [node for node in nodes if node['role'] == Node.WORKER or node['role'] == Node.INFRA]
-    master_nodes = [node for node in nodes if node['role'] == Node.MASTER or node['role'] == Node.BOOTSTRAP]
-    create_config(worker_nodes, master_nodes)
+    configure_haproxy(config)
     
 
 def read_config(config_path):
     with open(config_path, 'r') as file:
         return json.load(file)
+    
+def configure_haproxy(config):
+    subprocess.call("yum -y install haproxy", shell=True)
+    create_config(config)
+    subprocess.call("systemctl enable haproxy", shell=True)
+    subprocess.call("systemctl restart haproxy", shell=True)
+
 
 # Use standard library to create, because you will never know what network environment is.
-def create_config(worker_nodes, master_nodes):
+def create_config(config):
     print("Creating the configuration ...")
+
+    nodes = config['nodes']
+    worker_nodes = [node for node in nodes if node['role'] == Node.WORKER or node['role'] == Node.INFRA]
+    master_nodes = [node for node in nodes if node['role'] == Node.MASTER or node['role'] == Node.BOOTSTRAP]
 
     content = ''
     content += generate_rule(master_nodes, "openshift-api-server", "6443")
     content += generate_rule(master_nodes, "machine-config-server", "22623")
     content += generate_rule(worker_nodes, "ingress-https", "443")
     content += generate_rule(worker_nodes, "ingress-http", "80")
-    
-    if os.path.exists(CONFIG_PATH):
-        raise('Config already exists')
-    shutil.copyfile('./haproxy.cfg.tpl', CONFIG_PATH)
-    with open(CONFIG_PATH, 'w') as file:
-        file.write(content)
+    save_config(content)
 
 def generate_rule(nodes, name, port):
     content = CONTENT_PATTERN.format(name, port)
@@ -57,6 +62,22 @@ def generate_rule(nodes, name, port):
         content += '    server {0}    {1}:{2} check\n'.format(node['name'], node['ip'], port)
     return content
 
+def save_config(content):
+    if os.path.exists(CONFIG_PATH):
+        raise FileExistsError('Config already exists')
+
+    template_path = find_filepath('.', 'haproxy.cfg.tpl')
+    shutil.copyfile(template_path, CONFIG_PATH)
+    with open(CONFIG_PATH, 'w') as file:
+        file.write(content)
+
+def find_filepath(path, name):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(name):
+                return os.path.join(root, file)
+    
+    raise FileNotFoundError('Can not find {}'.format(name))
 
 if __name__ == '__main__':
     main()
